452f1686e3bd5e9d8d0a5199c9139c87b876d55b
The-creator63 <eshantaqi@icloud.com>
2025-02-02 18:00:24 +0100

    Add files via upload

--- multiplication.py
+++ multiplication.py
@@ -14,4 +14,21 @@ num = IntVar()
 numbers = Combobox(root,textvariable = num,width = 5,state = "readonly")
 numbers["values"] = tuple(range(101))
 numbers.grid(row = 2,column= 1)
+
+#radiobutton creation
+endval = IntVar()
+r10 = Radiobutton(root,text = "10",variable = endval,value = 10)
+r20 = Radiobutton(root,text = "20",variable = endval,value = 20)
+r30 = Radiobutton(root,text = "30",variable = endval,value = 30)
+endval.set(10)
+
+r10.grid(row = 4,column = 2)
+r20.grid(row = 5,column = 2)
+r30.grid(row = 6,column = 2)
+
+def multtable():
+    tables = ""
+    for i in range(1,endval.get() + 1):
+        multiply = num.get()*i
+        tables += str(num.get()) + " X " + str(i) + " = " + str(multiply) + "\n"
 root.mainloop()
